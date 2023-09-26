1fe30b5139a205b5b9356c01e503e9422b27c17d
Gsak79 <gonchissn@gmail.com>
2023-09-26 01:25:14 -0500

    feat: add connection to discovery server

--- src/main/java/com/docseeker/patient/api/PatientController.java
+++ src/main/java/com/docseeker/patient/api/PatientController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
-@RequestMapping("api/patients")
+@RequestMapping("api/v1/patients")
 @AllArgsConstructor
 public class PatientController {
   private final PatientService patientService;
